35c0a845cd4c3e016605b54fa5cb69303560338c
[Dliammc] <[dliammccraw@gmail.com]>
2025-06-21 19:54:49 -0400

    Updated version to v6.0.3

--- PyCt6/__init__.py
+++ PyCt6/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "6.0.2"
+__version__ = "6.0.3"
 __author__ = "D. Liam Mc."
-__date__ = "June 17, 2025"
+__date__ = "June 21, 2025"
 
 from .windows import CMainWindow
 from .windows import CTopLevel
