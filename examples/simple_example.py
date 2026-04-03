d81eb12c1bc6bcf9b034c591a2484682f89cf1ab
[Dliammc] <dliammc@duck.com>
2026-04-02 22:12:06 -0400

    updated simple_example.py

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -2,12 +2,10 @@ from PyCt6 import CMainWindow, CButton, set_appearance_mode, set_color_theme
 from PySide6.QtWidgets import QApplication, QVBoxLayout
 from PySide6.QtCore import Qt
 
-set_appearance_mode("system")
-set_color_theme("blue")
 
 class MainWindow(CMainWindow):
     def __init__(self):
-        super().__init__(width=400, height=250, title="CMainWindow", background_color="rgb(30,30,30)")
+        super().__init__(width=400, height=250, title="CMainWindow")
 
         self.main_layout = QVBoxLayout()
         self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
